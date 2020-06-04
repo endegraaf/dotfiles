d33b328816c5aa8abb2a7eadcf7a74ff18789e5f
Eric de Graaf <endegraaf@gmail.com>
2020-06-04 13:38:07 +0200

    add script to create symlinks

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo Hello World
+echo Make Symlinks to ~/git/dotfiles/ ... 
+ln -s ~/git/dotfiles/config/autorandr/ ~/.config/autorandr
+ln -s ~/git/dotfiles/config/copyq/copyq.conf ~/.config/copyq/copyq.conf
+ln -s ~/git/dotfiles/config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+ln -s ~/git/dotfiles/config/i3 ~/.config/i3
+ln -s ~/git/dotfiles/config/obs-studio ~/.config/obs-studio
+ln -s ~/git/dotfiles/config/picom ~/.config/picom
+ln -s ~/git/dotfiles/config/polybar ~/.config/polybar
+ln -s ~/git/dotfiles/config/compton.conf ~/.config/compton.conf
+ln -s ~/git/dotfiles/config/kdeglobals ~/.config/kdeglobals
+ln -s ~/git/dotfiles/config/kdenliverc ~/.config/kdenliverc
+ln -s ~/git/dotfiles/xfiles/ ~/.xfiles
+ln -s ~/git/dotfiles/vimrc ~/.vimrc
+ln -s ~/git/dotfiles/Xmodmap ~/.Xmodmap 
+ln -s ~/git/dotfiles/config/touchpadrc ~/.config/touchpadrc
+ln -s ~/git/dotfiles/config/touchpadxlibinputrc ~/.config/touchpadxlibinputrc
+
