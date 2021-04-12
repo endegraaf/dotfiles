83adcfaf647b6376645d45cb89cfcc23cb6c1a51
Eric de Graaf <endegraaf@gmail.com>
2021-04-12 22:43:07 +0200

    Delete askpass.sh

--- askpass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# source: https://wiki.archlinux.org/index.php/KDE_Wallet#Using_the_KDE_Wallet_to_store_ssh_key_passphrases
-# also need to install ksshaskpass executable.
-# ~/.config/plasma-workspace/env/askpass.sh
-export GIT_ASKPASS='/usr/bin/ksshaskpass'
