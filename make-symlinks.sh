9e34945df97a8b0893538c02f20ef0c541feed64
Eric de Graaf <endegraaf@gmail.com>
2021-05-23 22:33:24 +0200

    Update after fresh i3 install on demo environment

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -7,18 +7,13 @@ ln -s ~/git/dotfiles/bashrc ~/.bashrc
 ln -s ~/git/dotfiles/config/autorandr/ ~/.config/autorandr
 ln -s ~/git/dotfiles/config/copyq/copyq.conf ~/.config/copyq/copyq.conf
 ln -s ~/git/dotfiles/config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
-ln -s ~/git/dotfiles/config/i3 ~/.config/i3
-ln -s ~/git/dotfiles/config/obs-studio ~/.config/obs-studio
+ln -s ~/git/dotfiles/config/i3/config ~/.config/i3/config
 ln -s ~/git/dotfiles/config/picom ~/.config/picom
 ln -s ~/git/dotfiles/config/polybar ~/.config/polybar
 ln -s ~/git/dotfiles/config/compton.conf ~/.config/compton.conf
-ln -s ~/git/dotfiles/config/kdeglobals ~/.config/kdeglobals
-ln -s ~/git/dotfiles/config/kdenliverc ~/.config/kdenliverc
 ln -s ~/git/dotfiles/xfiles/ ~/.xfiles
 ln -s ~/git/dotfiles/vimrc ~/.vimrc
 ln -s ~/git/dotfiles/Xmodmap ~/.Xmodmap 
-ln -s ~/git/dotfiles/config/touchpadrc ~/.config/touchpadrc
-ln -s ~/git/dotfiles/config/touchpadxlibinputrc ~/.config/touchpadxlibinputrc
 else
         echo Missing the dotfiles dir in the git folder!
 fi
@@ -26,3 +21,4 @@ echo Plug for Vim plugin management
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
